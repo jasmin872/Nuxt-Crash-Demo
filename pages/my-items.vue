96f136ca2ff272b5dd4fb42b9f2ecc953b84b77c
Raju Khunt <rajukhunt15@gmail.com>
2021-05-09 11:33:48 +0530

    changes

--- pages/my-items.vue
+++ pages/my-items.vue
@@ -1,15 +1,15 @@
 <template>
     <div>
-            my items page
+        my items page
     </div>
 </template>
 
 <script>
     export default {
-        
+        layout: "no-nav"
     }
 </script>
 
-<style lang="scss" scoped>
+<style scoped>
 
 </style>
